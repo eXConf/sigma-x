c40596b9bf7adcdd0ba76b16fc3e737651fd8ca5
eXConf <sergey.sosedkin@gmail.com>
2020-06-05 20:57:10 +0300

    Removed option to reload page

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const template = [
     label: 'Игра',
     submenu: [
       { label: 'Новая игра...', click() {openNewGameWindow();} },
-      { label: 'Перезагрузить', role: 'reload' },
+      //{ label: 'Перезагрузить', role: 'reload' },
       isMac ? { label: 'Выход', role: 'close' } : { label: 'Выход', role: 'quit' }
     ]
   },
